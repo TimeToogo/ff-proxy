1b063bc81888dcfc1169ccffd8f866528dcad377
Elliot Levin <elliotlevin@hotmail.com>
2020-04-02 18:18:11 +1100

    Fix import reference

--- client/python/setup.py
+++ client/python/setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -12,7 +12,7 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/TimeToogo/ff-proxy",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
