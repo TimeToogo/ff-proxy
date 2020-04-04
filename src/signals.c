e1328ebcaaf4434dfd872d027684d0368da4b9c4
Elliot Levin <elliotlevin@hotmail.com>
2020-04-04 12:29:56 +1100

    signals: supress unused result warning in SIGINT handler

--- src/signals.c
+++ src/signals.c
@@ -8,6 +8,6 @@
 void ff_sigint_handler(int signal __attribute__((unused)))
 {
     char message[] = "Interrupt signal received, terminating process!\n";
-    write(STDOUT_FILENO, message, sizeof(message));
+    int result __attribute__((unused)) = write(STDOUT_FILENO, message, sizeof(message));
     exit(1);
 }
