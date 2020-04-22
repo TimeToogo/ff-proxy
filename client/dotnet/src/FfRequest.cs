39d2192393cc7698ec0f07e3f672616b3fcdbac7
Elliot Levin <elliotlevin@hotmail.com>
2020-04-22 22:34:38 +1000

    Migrate dotnet client to use secure request options

--- client/dotnet/src/FfRequest.cs
+++ client/dotnet/src/FfRequest.cs
@@ -7,6 +7,8 @@ namespace FfClient
     {
         public FfRequestVersion Version { get; set; }
         public UInt64 RequestId { get; set; }
+        public List<FfRequestOption> SecureOptions { get; set; } = new List<FfRequestOption>();
+
         public List<FfRequestOption> Options { get; set; } = new List<FfRequestOption>();
         public byte[] Payload;
     }
