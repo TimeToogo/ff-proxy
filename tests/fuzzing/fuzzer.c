3902f7ecfb878ca173fe06b3a0600dca1ab8b9a9
Elliot Levin <elliotlevin@hotmail.com>
2020-04-25 11:22:18 +1000

    Add fuzzing call to parse options from payload and add corpus

--- tests/fuzzing/fuzzer.c
+++ tests/fuzzing/fuzzer.c
@@ -8,8 +8,12 @@ extern int LLVMFuzzerTestOneInput(const uint8_t *data, size_t length)
     ff_set_logging_level(FF_ERROR);
 
     struct ff_request *request = ff_request_alloc();
-    
+
     ff_request_parse_chunk(request, (uint32_t)length, (void*)data);
+
+    if (request->state == FF_REQUEST_STATE_RECEIVED) {
+        ff_request_parse_options_from_payload(request);
+    }
 
     ff_request_free(request);
 
